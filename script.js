8413c2ea585bdcc870369736270e48434200656d
Bianca Pena <biancalessapena@gmail.com>
2023-01-22 23:39:30 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// const celular = {
+//     cor: 'preto',
+//     ligar: function() {
+//         const mensagem = "Ligando"
+//         alert(mensagem)
+//     }
+// }
+
+// celular.ligar()
+
+// document.querySelector('input').click()
+
+// const data = {
+//     run: ['01-01', '01-02', '01-03', '01-04', '01-06'],
+//     takePills: ['01-03'],
+//     journal: ['01-02']
+// }
+
+
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert('Dia já incluso! ❌')
+        return
+    }
+
+    alert('Adicionado com sucesso! ✅')
+    nlwSetup.addDay(today)
+}
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
+
